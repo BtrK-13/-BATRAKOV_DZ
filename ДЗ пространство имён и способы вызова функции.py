1b7b1bb2ba93bcf012b36c255e36f8ac98056e88
BtrK13 <161844687+BtrK-13@users.noreply.github.com>
2024-03-25 00:53:31 +0400

    Add files via upload

---
+++ ДЗ пространство имён и способы вызова функции.py
@@ -0,0 +1,12 @@
+def test():
+    a=10
+    b=20
+    print("test а = ",a)
+    print("test b = ",b)
+test()
+print()
+def test2(a=1,b=2,c=3):
+    print('test2 a =', a)
+    print('test2 b =', b)
+    print('test2 c =', c)
+test2()
